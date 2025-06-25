69d0e9671a65046827f1c5127788fbda87773b8f
ribeirosofia <ribeiro.sofia.sr@gmail.com>
2025-06-24 21:29:08 -0300

    "feat: create new user"

--- src/library/Library.java
+++ src/library/Library.java
@@ -1,9 +1,48 @@
 package library;
 
-public class Library {
-    Book bookId;
-    User name;
-    public static void main(String[] args) {
+import javax.naming.InvalidNameException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Locale;
+import java.util.Scanner;
 
+public class Library {
+
+    private List<Book> books = new ArrayList<>();
+    private List <Author> authors = new ArrayList<>();
+    private List<Loan> loans = new ArrayList<>();
+    private List<User> users = new ArrayList<>();
+
+    Scanner scanner = new Scanner(System.in);
+
+    public void handleUserLoginFlow(){
+        System.out.println("Você já é cadastrado? (sim/nao)");
+        String answer = scanner.next();
+        User newUser = new User();
+
+        if(answer.equalsIgnoreCase("nao")){
+            newUser.insertNewUserData();
+            users.add(newUser);
+        } else {
+            System.out.println("Digite seu email: ");
+            String email = scanner.next();
+
+            boolean found = false;
+
+            for(User user : users){
+                if(user.getEmail().equalsIgnoreCase(email)){
+                    System.out.println("Bem vindo(a) de volta, " + user.getName());
+                    found = true;
+                    break;
+                }
+            }
+
+            if(!found){
+                System.out.println("Usuário não encontrado. Vamos fazer seu cadastro");
+                newUser.insertNewUserData();
+                users.add(newUser);
+            }
+        }
     }
+
 }
