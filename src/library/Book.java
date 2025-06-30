43de16abe8b10279adeea8a852b965d5ae36c15f
ribeirosofia <ribeiro.sofia.sr@gmail.com>
2025-06-30 13:47:42 -0300

    "feat: register author"

--- src/library/Book.java
+++ src/library/Book.java
@@ -5,8 +5,20 @@ import java.time.LocalDate;
 public class Book {
     Integer bookId;
     String title;
-    Author authorId;
+    Author author;
     Boolean available;
     LocalDate DateRegister;
-    LocalDate DateUpdate;
+    LocalDate DateReturned;
+
+    public String getTitle() { return title;    }
+
+    public Author getAuthor() { return author; }
+
+    public Boolean getAvailable() { return available; }
+
+    public LocalDate getDateRegister() { return DateRegister;   }
+
+    public LocalDate getDateReturned() {  return DateReturned;  }
+
+
 }
